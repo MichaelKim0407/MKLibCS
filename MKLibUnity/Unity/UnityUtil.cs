b2f8ebf66897659237407439a8ee4c911e6f377e
Michael Kim <jinzheng19930407@sina.com>
2017-01-29 18:02:13 +0800

    unity extended methods

---
+++ MKLibUnity/Unity/UnityUtil.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace MKLibCS.Unity
+{
+    /// <summary>
+    /// </summary>
+    public static class UnityUtil
+    {
+        /// <summary>
+        /// </summary>
+        /// <param name="gameObject"></param>
+        public static void DontDestroyOnLoad(this GameObject gameObject)
+        {
+            Object.DontDestroyOnLoad(gameObject);
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="gameObject"></param>
+        public static void Destroy(this GameObject gameObject)
+        {
+            Object.Destroy(gameObject);
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="gameObject"></param>
+        public static void SafeDestroy(this GameObject gameObject)
+        {
+            gameObject.transform.SetParent(null);
+            gameObject.SetActive(false);
+            gameObject.Destroy();
+        }
+    }
+}
